95b80c6e4c4bd3185c3ad598268465b2cac65a0f
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2023-02-27 13:37:54 +0200

    home

--- frontend/myapp/src/components.js/home.js
+++ frontend/myapp/src/components.js/home.js
@@ -1,179 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import App from "../App";
-
 function Home() {
   return (
     <App>
       <div id="home" className="active-section section">
-        <h1>Welcome to our landing page</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
-          lectus aliquam, congue felis nec, sollicitudin enim. Nulla facilisi.
-          Nullam vel risus ut justo lacinia facilisis ac ut ipsum. Sed euismod
-          quis lectus a tempor.
-        </p>
+        <section class="banner"></section>
+
+        <section class="homeservice">
+          <h1>Experienced Engineers Delivering Quality Solutions</h1>
+          <div class="homeservice">
+            <h2>Our Services</h2>
+            <ul>
+              <li>
+                <h3>Web Development</h3>
+                <p>
+                  We offer custom web development services using various
+                  frameworks such as React, Angular, and Vue.
+                </p>
+              </li>
+              <li>
+                <h3>Database Design</h3>
+                <p>
+                  We provide database design and development services using
+                  various platforms such as MySQL and MongoDB.
+                </p>
+              </li>
+              <li>
+                <h3>Other Services</h3>
+                <p>
+                  We provide alot of more <Link to="/services">services</Link>
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section>
       </div>
     </App>
   );
